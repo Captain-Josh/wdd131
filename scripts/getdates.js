77775df327baeee81cad55915a7407e5d5200554
Captain-Josh <jnweke54@gmail.com>
2026-03-07 21:31:37 +0100

    updated w01 assignment: home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,7 @@
 
   document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-  document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
+  document.getElementById('lastmodified').textContent = `Last modified: ${document.lastModified}`;
 
 //   document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
 
