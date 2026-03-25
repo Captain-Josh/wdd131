614f7b20194a249e8ec7dd78dfbe01e0c62403b2
Captain-Josh <jnweke54@gmail.com>
2026-03-25 16:51:34 +0100

    completed picture album enhancement assignment.

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -20,64 +20,63 @@ const temples = [
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
     area: 11500,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    src:"images/temple/aba-nigeria-temple.webp"
   },
+
   {
     templeName: "Manti Utah",
     location: "Manti, Utah, United States",
     dedicated: "1888, May, 21",
     area: 74792,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  src:"images/temple/manti-temple.webp"
   },
+
   {
     templeName: "Payson Utah",
     location: "Payson, Utah, United States",
     dedicated: "2015, June, 7",
     area: 96630,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    src:"images/temple/payson-utah-temple.webp"
   },
+
   {
     templeName: "Yigo Guam",
     location: "Yigo, Guam",
     dedicated: "2020, May, 2",
     area: 6861,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    src:"images/temple/yigo-guam-temple.webp"
   },
+
   {
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, United States",
     dedicated: "1974, November, 19",
     area: 156558,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    src:"images/temple/washington-dc-temple.webp"
   },
+
   {
     templeName: "Lima Perú",
     location: "Lima, Perú",
     dedicated: "1986, January, 10",
     area: 9600,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    src:"images/temple/lima-peru-temple.webp"
   },
+
   {
     templeName: "Mexico City Mexico",
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
     area: 116642,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    src:"images/temple/mexico-city-temple.webp"
   },
+
   {
     templeName: "Salt Lake City",
     location: "Salt Lake City, UTAH",
     dedicated: "1893, April, 6",
     area: 382207,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-city-utah/400x250/salt-lake-temple-37762.jpg"
+    src:"images/temple/salt-lake-temple.webp"
   },
 
   {
@@ -85,8 +84,7 @@ const temples = [
     location: "Accra, Ghana",
     dedicated: "2003, January, 11",
     area: 17500,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/400x250/accra-ghana-temple-758797-wallpaper.jpg"
+    src:"images/temple/accra-ghana-temple.webp"
   },
 
   {
@@ -94,8 +92,7 @@ const temples = [
     location: "Rome, Italy",
     dedicated: "10, March, 10",
     area: 41010,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rome-italy/2019/400x250/2-Rome-Temple-2190090.jpg"
+    src:"images/temple/rome-temple.webp"
   }
 ];
 
@@ -110,7 +107,7 @@ function displayAllTemples(temples){
     for (const temple of temples){
         let templeCard = `
                 <figure>
-            <img src="${temple.imageUrl}"
+            <img src="${temple.src}"
                  alt="${temple.templeName}"
                  loading="lazy">
 
