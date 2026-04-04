896a2591d57545064e065dd94e74728c3b89928e
Captain-Josh <jnweke54@gmail.com>
2026-04-04 16:30:08 -0400

    product review form completed.

--- scripts/form.js
+++ scripts/form.js
@@ -1,1 +1,46 @@
-document.querySelector(".currentyear").textContent = new Date();
+document.querySelector(".currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastmodified").textContent = `Last Modified: ${document.lastModified}`;
+
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+function populateProductName(products){
+    const productName = document.getElementById("productname");
+    productName.innerHTML+="";
+    for (product of products){
+        let productCard = `<option value="${product.id}">${product.name}</option>`
+        productName.innerHTML+= productCard;
+    }
+}
+populateProductName(products)
+
+
+
+
+
